2c591ad138bccff59ed672f3abf5f00de85dc947
Naren Shridhar <naren.shridhar@infor.com>
2019-01-04 09:34:32 -0500

    Chapter2Problem2.java

---
+++ Chapter2Problem2.java
@@ -0,0 +1,20 @@
+// Chapter 2 Page 105
+// Programming Challenges Problem #2
+
+public class Chapter2Problem2
+{
+   public static void main(String[] args)
+      {
+         String firstName = "Naren";
+         String middleName; middleName = "Volcano";
+         String lastName; lastName = "Shridhar";
+         
+         char firstInitial; firstInitial = 'N';
+         char middleInitial; middleInitial = 'V';
+         char lastInitial; lastInitial = 'S';
+         
+         System.out.println("Hi my name is " + (firstName + " ") + (middleName + " ") + (lastName));
+         System.out.println("My initials are " + (firstInitial + " ") + (middleInitial + " ") + (lastInitial + " "));
+         
+      }
+}   
